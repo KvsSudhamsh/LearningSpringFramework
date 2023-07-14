91d77bba27ed2374a7c9422b4e24a0a0d3a4d8b7
vishnukvs <vishnu.kvs202@gmail.com>
2023-07-13 19:14:01 -0700

    implemented the setter and getter as part of setter injection

--- src/main/java/com/programming/techie/EmailApplication.java
+++ src/main/java/com/programming/techie/EmailApplication.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 //import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class EmailApplication {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContext=
+                new ClassPathXmlApplicationContext("beans.xml");
+        /*ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);*/
 
         EmailClient emailClient=applicationContext.getBean("emailClient", EmailClient.class);
        emailClient.sendEmail("Hey "+ "This is my first email message.");
